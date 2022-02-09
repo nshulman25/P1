a27ba6c9ace55c1c56dc4d504616edaceb9423a8
nshulman25 <nshulman25@gmail.com>
2022-02-08 19:07:40 -0500

    Comments and  parity bit implementation

--- encrypt.java
+++ encrypt.java
@@ -18,22 +18,25 @@ public class encrypt{
 
         String padded_Input[][] = padding(encryption);
         String shift_Row[][] = shiftRows(padded_Input);
+        String parity_Bit[][] = parityBit(shift_Row);
     }
 
-    // readFile method
-    // Reads desired file based on param and converts its content to a string and returns it.
+    /**
+     * Reads desired file based on param and converts its content to a string and returns it.
+     * @param fileName
+     * @return
+     */
     private static String readFile(String fileName){
         
-        String data = null;
+        String data = "";
        
         // Takes the pathname of the file finds it and scans through it and returns the data
         try {
             File pathname = new File(fileName);
             Scanner read = new Scanner(pathname);
             while (read.hasNextLine()){
-                data = read.nextLine();
+                data += read.nextLine();
             }
-            read.close();
         } catch (Exception e) {
             System.out.println("no input file found");
             e.printStackTrace();
@@ -42,8 +45,13 @@ public class encrypt{
         return data;
     }
 
-    // writeFile method
-    // writes the desired string to a file named "output.txt"
+    /**
+     *  writes the desired string to a file named "output.txt"
+     * @param input
+     * @param append
+     * @return
+     * @throws IOException
+     */
     private static File writeFile(String input, Boolean append) throws IOException{
         
         // Creates a file named output.txt, a file writer, and a print writer
@@ -58,8 +66,11 @@ public class encrypt{
         return null;
     }
 
-    // preProccessng method
-    // Removes all white space and punctuations from desired string and returns it.
+    /**
+     * Removes all white space and punctuations from desired string and returns it.
+     * @param input
+     * @return
+     */
     private static String preProcessing(String input){
         
         // replaces all whitespace
@@ -71,8 +82,12 @@ public class encrypt{
         return input;
     }
 
-    // vignereCipher method
-    // Encrypts the cleaned plain text using the provided key through the vignere cipher and returns it.
+    /**
+     * Encrypts the cleaned plain text using the provided key through the vignere cipher and returns it.
+     * @param input
+     * @param key
+     * @return
+     */
     private static String vignereCipher(String input, String key){
         
         String encipher = "";
@@ -101,9 +116,13 @@ public class encrypt{
         return encipher;
     }
 
-    // padding method
-    // takes the encrypted message and pads A till the length is divisible by 16.
-    // and add its to a 2d array.
+    /**
+     * Takes the encrypted message and pads A till the length is divisible by 16 
+     * and add its to a 2d array.
+     * @param input
+     * @return
+     * @throws IOException
+     */
     private static String[][] padding(String input) throws IOException{
         
         // padds the string with 'A' to have a length divisible by 16
@@ -140,10 +159,20 @@ public class encrypt{
         return padded_input;
     } 
     
-    private static String[][] shiftRows(String[][] input){
+    /**
+     * Shift all the rows in the array depending on their row number 
+     * @param input
+     * @return an array of string
+     * @throws IOException
+     */
+    private static String[][] shiftRows(String[][] input) throws IOException{
         
+        writeFile("ShiftRows:" + "\n", true);
+        
+        // Counter to determine row number(0-3)
         int counter = 0;
        
+        // Creates a temporary copy of the 2d array
         String[][] temp = new String[input.length][];
         for(int row = 0; row < input.length; row++){
             temp[row] = new String[input[row].length];
@@ -158,67 +187,71 @@ public class encrypt{
         // loops through the 2d array (rows, right to left; then down one column)
         for(int row = 0; row < input.length; row++){
             for(int column = 0; column < input[row].length; column++){
+               
                 // 2nd row shift
+                // Shifts all characters 1 to the left in the row and wraps around
                 if(counter == 1 ){
-                    
                     
                     if(column == 3){
                         input[row][column] = temp[row][0];
-                        System.out.print(input[row][column]);
+                        writeFile(input[row][column], true);
                         break;
                     }
 
                     input[row][column] = input[row][column+1];
-                    System.out.print(input[row][column]);
+                    writeFile(input[row][column], true);
                 }
                 
                 // 3rd row shift
+                // Shifts all characters 2 to the left in the row and wraps around
                 else if(counter == 2){
                     
                     if(column == 2){
                         input[row][column] = temp[row][0];
-                        System.out.print(input[row][column]);
+                        writeFile(input[row][column], true);
                         continue;
                     }
                     if(column == 3){
                         input[row][column] = temp[row][1];
-                        System.out.print(input[row][column]);
+                        writeFile(input[row][column], true);
                         continue;
                     }
                     
                     input[row][column] = input[row][column+2];
-                    System.out.print(input[row][column]);
+                    writeFile(input[row][column], true);
                 }
 
-                // 4th tow shift
+                // 4th row shift
+                // Shifts all characters 3 to the left in the row and wraps around
                 else if(counter ==3){
                    
                     if(column == 1){
                         input[row][column] = temp[row][0];
-                        System.out.print(input[row][column]);
+                        writeFile(input[row][column], true);
                         continue;
                     }
                     if(column == 2){
                         input[row][column] = temp[row][1];
-                        System.out.print(input[row][column]);
+                        writeFile(input[row][column], true);
                         continue;
                     }
                     if(column == 3){
                         input[row][column] = temp[row][2];
-                        System.out.println(input[row][column]);
+                        writeFile(input[row][column] + "\n", true);
                         continue;
                     }
                     
                     input[row][column] = input[row][column+3];
-                    System.out.print(input[row][column]);
+                    writeFile(input[row][column], true);
                 
-                // 1st row shift    
+                // 1st row shift
+                // Keeps rows the same     
                 }else{
-                    System.out.print(input[row][column]);
+                    writeFile(input[row][column], true);
                 }
 
             }
-            System.out.println();
+            writeFile("\n", true);
             counter++;
 
             if (counter > 3){
@@ -226,6 +259,76 @@ public class encrypt{
             }
         }
  
-        return null;
+        return input;
+    }
+    
+
+    /**
+     * Determines if a binary number will have even or odd number of ones
+     * @param n
+     * @return (true == odd # of 1s) (false == even # of ones)
+     */
+    private static boolean parityBitFunction(int n){
+       
+        boolean paritybit = false;
+        while(n != 0){
+            n = n & (n-1);
+            paritybit = !paritybit;
+        }
+
+        return paritybit;
+    }
+
+    /**
+     * loops through a 2d array and if the strings value converted to a binary nummber 
+     * has an odd number of 1s adds a parity bit
+     * @param input
+     * @return 2d array of hexidecimal numbers as strings
+     * @throws IOException
+     */
+    private static String[][] parityBit(String[][] input) throws IOException{
+        writeFile("Parity: " + "\n", true);        
+        int pointer = 0;
+
+           // loops through the 2d array (rows, right to left; then down one column)
+           for(int row = 0; row < input.length; row++){
+            for(int column = 0; column < input[row].length; column++){
+                
+                String hex = null;
+                
+                // Converts the current value to its decimal representation
+                char c = input[row][column].charAt(0);
+                int n = (int) c;
+                
+                // if odd number of 1s add parity bit and convert to hex and add back to array
+                if(parityBitFunction(n) == true){
+                    
+                    hex = String.format("%x", n);
+                    int num = Integer.parseInt(hex, 16);
+                    String binary = Integer.toString(num, 2);
+                    
+                    binary = 1 + binary;
+
+                    int decimal = Integer.parseInt(binary, 2);
+                    hex = String.format("%x", decimal);
+                    
+                    input[row][column] = hex;
+                // if even convert back to hex and input in array
+                }else{
+                    hex = String.format("%x", (int) n);
+                    input[row][column] = hex;
+                } 
+
+                writeFile(input[row][column] + " ", true);
+                pointer++;
+
+            }
+            writeFile("\n", true); 
+            if(pointer%16 == 0){
+                writeFile("\n", true);
+            }       
+        }
+
+        return input;
     }
 }
